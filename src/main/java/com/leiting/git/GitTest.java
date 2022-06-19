907430783038484fdf557ad5683549190fe4ffb0
LLT <1422316216@qq.com>
2022-06-19 23:27:38 +0800

    hot-fix commit1

--- src/main/java/com/leiting/git/GitTest.java
+++ src/main/java/com/leiting/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello Git2");
         System.out.println("hot-fix");
+        System.out.println("hot-fix1");
     }
 
 }
